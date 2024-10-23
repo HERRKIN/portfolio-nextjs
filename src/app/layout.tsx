b11d1f81695f06ed09ecde677f1311b27f714aaf
José Andradez <joseandradez@Joses-MacBook-Pro.local>
2024-10-23 17:44:54 -0300

    add og metadata and image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,37 @@ import Blobs from "../components/blobs";
 import Header from "../components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "José Andradez - Developer Portfolio",
   description:
     "Developer portfolio",
+  openGraph: {
+    title: "José Andradez - Developer Portfolio",
+    description:
+      "Developer portfolio",
+    url: "https://herrkin.github.io/portfolio-nextjs/",
+    siteName: "José Andradez Portfolio",
+    images: [
+      {
+        url: "https://herrkin.github.io/portfolio-nextjs/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "José Andradez - Developer Portfolio",
+    description:
+      "Developer portfolio",
+    images: ["https://herrkin.github.io/portfolio-nextjs/og-image.png"],
+  },
 };
 
 export default function RootLayout({
